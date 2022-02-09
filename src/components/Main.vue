dfcad6cd324fa8864ff825a5e4a668d9240ca735
willweatherman <will@mathbot.com>
2022-02-09 08:31:14 -0500

    init

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
   <div class="BOX-ONE">
     <div class="small" style="display: none;">This website is not supported for this screen size please view on a larger device, We are working to fix this.</div>
     <div class="Header">
-      <img class="logo-one" src="../assets/logo.png">
+      <p class="logo-one H1">UMAD CLAN</p>
       <div class="nav-buttons">
         <div class="nav-button H5"v-on:click="scrollTo('Teams')">The Teams</div>
         <div class="nav-button H5" v-on:click="scrollTo('Roster')" >Roster</div>
@@ -16,7 +16,7 @@
 <!--         <div class="Content-One-Nav-Button H5" v-on:click="scrollTo('Register-Left')"><div class="buttonC">Apply Now</div></div>
  -->        <video id="video1"
     muted="muted">
-            <source src="../assets/UMADCLAN_Ninja_Intro.mp4"/>
+            <source src="../assets/UMADCLAN_Ninja_Intro.mp4" poster="../assets/UMADNINJA.jpg">
         </video>
       </div>
       
@@ -156,9 +156,13 @@ export default {
 }
 
 .logo-one {
-  height: 80%;
-  margin-left: 5%;
+  height: 60%;
+  margin-left: 5%!important;
+  margin-top: auto!important;
+  margin-bottom: auto!important;
   align-self: center;
+  color: rgb(230, 60, 40);
+  align-text: center;
 }
 
 .nav-buttons {
